b40ce169134ce187dfc4f4579ef3ea86a1c90b57
SUMIT GHOSH <sumitghsh98@gmail.com>
2022-07-12 14:25:01 +0530

    first JDBC PROGRAMME

---
+++ FirstJDBC.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+class FirstJDBC{
+
+    /**
+     * @param args
+     */
+    public static void main(String args[])
+{
+try
+{
+    //load the driver:
+    Class.forName("com.mysql.jdbc.Driver");
+    //creating a connection
+    String url="jdbc:mysql://localhost:3306/sanu";
+    String username="root";
+    String password="Sumit95#";
+
+    Connection con =DriverManager.getConnection(url,username,password);
+    if (con.isClosed())
+    {
+        System.out.println("Connection is Closed");
+    }
+    else
+    {
+       System.out.println("Connection is created"); 
+    }
+}
+catch(Exception e)
+    {
+        e.printStackTrace();
+    }
+}
+}
+
